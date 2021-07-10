84e4d6eed868c8cb2450e12916221e78ec52a473
Hp <ikhan6334@gmial.com>
2021-07-11 02:36:41 +0530

    commiting

---
+++ Samplemaven_Framework2/src/test/java/com/crm/vtiger/ProductTest/CreateProductTest.java
@@ -0,0 +1,20 @@
+package com.crm.vtiger.ProductTest;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+	
+	@Test
+	public void createProductWithNameTest()
+	{
+		System.out.println("execute create with name");
+	}
+	
+	
+	@ Test
+	public void createProductWithId()
+	{
+		System.out.println("execute create with id");
+	}
+
+}
---
+++ Samplemaven_Framework2/src/test/java/com/crm/vtiger/ProductTest/SearchProductTest.java
@@ -0,0 +1,20 @@
+package com.crm.vtiger.ProductTest;
+
+import org.testng.annotations.Test;
+
+public class SearchProductTest {
+	@Test
+	public void searchProductWithNameTest()
+	{
+		System.out.println("execute searchProductWithName");
+	}
+	
+	
+	@ Test
+	public void searchProductWithId()
+	{
+		System.out.println("execute searchProductWithId");
+	}
+
+
+}
